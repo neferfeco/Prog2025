b410a94371da04f55182634b930acf719f5ded95
Mona Lisa <fpiller@gmail.com>
2025-09-16 11:36:22 +0200

    xcvxcv

--- elso.py
+++ elso.py
@@ -1,9 +1,24 @@
+
 import random
 
+szamok = []
 
-pass
+# 100 elemű lista feltöltése 1-99 közötti véletlendzámokkal
+for i in range(100):
+    #.... szám generálás
+    rszam = random.randint(1, 100)
+
+    # szám elhelyezése a listába 
+    szamok.append(rszam)
+
+#Ellenőrzés
+print(szamok)
 
 
 
+#egyszámjáték
+jatek_szam = 0
+nem_talaltDB = 0
 
-
+#kitalalandó szám beállítása
+kitalalando_szam=input()
