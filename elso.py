6874897d763a48330ee0e2df446cca34085a148e
Mona Lisa <fpiller@gmail.com>
2025-09-23 12:24:20 +0200

    éjk

--- elso.py
+++ elso.py
@@ -3,9 +3,8 @@ import random
 
 szamok = []
 
-# 100 elemű lista feltöltése 1-99 közötti véletlendzámokkal
+# 100 elemű lista feltöltése 1-99 közötti véletlenszámokkal
 for i in range(100):
-    #.... szám generálás
     rszam = random.randint(1, 100)
 
     # szám elhelyezése a listába 
@@ -15,10 +14,26 @@ for i in range(100):
 print(szamok)
 
 
-
-#egyszámjáték
-jatek_szam = 0
-nem_talaltDB = 0
-
 #kitalalandó szám beállítása
-kitalalando_szam=input()
+kitalalando_szam = szamok[random.randint(0, len(szamok))]
+
+tipp = int(input("Kérek egy egész számot [1-100]: "))
+
+if(tipp == kitalalando_szam):
+    print("Gratulálok, eltaláltad a számot!")
+elif(tipp < kitalalando_szam):
+    print("A kitalálandó szám nagyobb!")
+else:
+    print("A kitalálandó szám kisebb!")
+
+while(tipp != kitalalando_szam):
+    tipp = int(input("Kérek egy egész számot [1-100]: "))
+    
+    if(tipp == kitalalando_szam):
+        print("Gratulálok, eltaláltad a számot!")
+    elif(tipp < kitalalando_szam):
+        print("A kitalálandó szám nagyobb!")
+    else:
+        print("A kitalálandó szám kisebb!")
+
+
