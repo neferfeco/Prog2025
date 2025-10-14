06cb45f8e11e42127c3ee3d3fe98ae01a0d26ae8
Mona Lisa <fpiller@gmail.com>
2025-10-14 11:31:47 +0200

    v2-b_hibás

--- fajlTeszt.py
+++ fajlTeszt.py
@@ -1,20 +1,83 @@
 
+verseny_adatok = []
 
 try:
-    with open("F1-2024dec1.csv", encoding="utf-8") as fajl:
-        f = fajl.read()
-        
-        n = 4 / 0
-        
-        print(f)
-        print(n)
+    with open("F1-2024dec.csv", encoding="utf-8") as fajl:
+        verseny_adatok = fajl.readlines()
 
 except Exception as ex:
     print(f"Halihóóóóó!: Hiba oka: {ex}")
 except FileNotFoundError:
     print("Hiba a fájl megnyitása közben!")
-except ZeroDivisionError:
-    print("Ne ossz 0-val!")
+
+
+    """
+    1. [X] Megszámolás
+    2. [X] Eldöntés 1
+       [] Eldöntés 2
+    3. [] Kiválasztás
+    4. [] Keresés
+    5. [] Sorozatszámítás, összegzés
+    6. [] Minimum/maximum kiválasztás
+    
+    7. [] Másolás
+    8. [] Kiválogatás
+    9. [] Szétválogatás
+    10.[] Metszet
+    11.[] Únió
+    
+    12. Rendezés
+        [] Egyszerű cserés rendezés
+        [] Buborékos
+        [] Minimum kiválasztásos
+    """
+
+
+# -----------------------------------------------
+
+# 1. Hány versenyző nem szerzett még pontot?
+db = 0
+
+for i in range(1, len(verseny_adatok)):
+    if(int(verseny_adatok[i].split(',')[1]) == 0):
+        db = db + 1
+
+print(f"{db} versenyző nem szerzett még pontot.\n")
+
+
+# 2.A Van-e Fernando nevű versenyző?
+
+i = 0
+while (i<len(verseny_adatok)and "Fernando" not in verseny_adatok[i]):
+    i=i+1
+if (i<len(verseny_adatok)):
+    print("Van ilyen versenyző")
+else:
+    print("Nincs ilyen versenyző")  
+
+
+# 2.B Mindenki szerzett már 90 pontot?
+i=0
+while (i<len(verseny_adatok and int(verseny_adatok[i].split(",")[1]))>=90):
+    i=i+1
+if i==len(verseny_adatok):
+    print("van")
+else:
+    print("nem")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
