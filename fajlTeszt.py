e66122425f11166113239078c32c1cf3778f3b83
Mona Lisa <fpiller@gmail.com>
2025-11-05 08:49:25 +0100

    1105

--- fajlTeszt.py
+++ fajlTeszt.py
@@ -18,7 +18,7 @@ except FileNotFoundError:
     3. [X] Kiválasztás
     4. [X] Keresés
     5. [x] Sorozatszámítás, összegzés
-    6. [] Minimum/maximum kiválasztás
+    6. [x] Minimum/maximum kiválasztás
     
     7. [] Másolás
     8. [] Kiválogatás
@@ -33,7 +33,7 @@ except FileNotFoundError:
     """
 
 
-# -----------------------------------------------
+# ---------------------------------------------------------
 
 # 1. Hány versenyző nem szerzett még pontot?
 db = 0
@@ -44,9 +44,9 @@ for i in range(1, len(verseny_adatok)):
 
 print(f"{db} versenyző nem szerzett még pontot.\n")
 
+# ---------------------------------------------------------
 
 # 2.A Van-e Fernando nevű versenyző?
-
 i = 0
 while (i<len(verseny_adatok)and "Fernando" not in verseny_adatok[i]):
     i=i+1
@@ -55,9 +55,9 @@ if (i<len(verseny_adatok)):
 else:
     print("Nincs ilyen versenyző")  
 
+# ---------------------------------------------------------
 
 # 2.B Mindenki szerzett már 90 pontot?
-
 i=1
 while i<len(verseny_adatok) and int(verseny_adatok[i].split(",")[1])>=90:
     i=i+1
@@ -65,6 +65,8 @@ if i==len(verseny_adatok):
     print("van")
 else:
     print("nem")
+
+# ---------------------------------------------------------    
     
 3. #Melyik istálló pilotája a Yuki Tsunoda?
 i=0
@@ -72,7 +74,9 @@ while verseny_adatok[i].split(",")[0]!="Yuki Tsunoda":
     i+=1
 print("Yuki Tsunoda a",verseny_adatok[i].split(",")[2])
 
-#melyik csapatban volt Pierre Gasly?
+# ---------------------------------------------------------
+
+#4. melyik csapatban volt Pierre Gasly?
 i=1
 while i<len(verseny_adatok) and "Pierre Gasly" not in verseny_adatok[i]:
     i=i+1
@@ -81,20 +85,75 @@ if i<len(verseny_adatok):
 else:
     print("Nincs ilyen versenyző ezért egyik csapatban sincs bennt!:(")
 
+# ---------------------------------------------------------
 
-
-#számolja ki a versenyzők pontszámainak átlagát
+#5. számolja ki a versenyzők pontszámainak átlagát
 S=0
 for i in range(1, len(verseny_adatok)):
     S+=int(verseny_adatok[i].split(',')[1])
 print(f"a versenyzők pontszámainak átlaga: {S/len(verseny_adatok)-1}")
-    
+
+# ---------------------------------------------------------
+
+#6. Max kiválasztás    
 maxi=1
 max=verseny_adatok[i].split(",")[1]
 for i in range(3,len(verseny_adatok)):
     if verseny_adatok[i]>verseny_adatok[maxi]:
         maxi=i
         max=verseny_adatok[i]
+
+# ---------------------------------------------------------
+        
+#6. Min kiválasztás
+mini=1
+min=verseny_adatok[i].split(",")[1]
+for i in range(2,len(verseny_adatok)):
+    if verseny_adatok[i]<verseny_adatok[mini]:
+        mini=i
+        min=verseny_adatok[i]
+print("A legkevesebb ponttal rendelkező versenyző: ",verseny_adatok[mini].split(",")[0])
+
+# ---------------------------------------------------------
+
+#8. KIVÁLOGATÁS (másik listába)
+#kik vannak a Mclaren istálloban
+db1=0
+masik_lista=[]
+for i in range(2,len(verseny_adatok)):
+    if verseny_adatok[i].split(",")[2].strip()=="McLaren":
+        db1=db1+1
+        masik_lista.append(verseny_adatok[i].split(",")[0])
+print("Ezek a személyek vannak a Mclaren istálloban:",masik_lista)
+
+
+
+
+
+
+
+
+#9. SZÉTVÁLOGATÁS
+# 
+
+
+
+
+
+
+
+
+#7. MÁSOLÁS
+#
+
+
+
+
+
+
+
+#12. RENDEZÉS: minimmumkiválasztásos
+
 
 
 
